385a7288b3d61e66b8ed1b13379790cdb12eb224
Son A. Pham <sonph@users.noreply.github.com>
2020-06-11 21:50:48 -0700

    Pop out window button in settings

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,13 @@ export default class App {
     this.uiData.settings.visible = false;
   }
 
+  popOut() {
+    window.open(
+      'index.html',
+      '_blank',
+      'width=400,height=600,resizable=no,scrollbars=no,menubar=no,location=no,status=no,toolbar=no');
+  }
+
   getUiData() {
     return this.uiData;
   }
